1207aae4fd7afbd1989b581273fc7b6c20a59e86
서주현 <beloved930223@gmail.com>
2023-02-28 17:33:28 +0900

    add: 도서대여 리팩토링 레포지토리에 추가

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,10 @@
-const router = require("express").Router();
-const { default: axios } = require("axios");
-const Book = require("../models/book");
+const router = require('express').Router();
+const { default: axios } = require('axios');
+const Book = require('../models/book');
 
-router.get("/", (req, res) => {
+router.get('/', async (req, res) => {
+  const page = req.query.page;
+
   if (req.query.isbn) {
     Book.find({ isbn: req.query.isbn })
       .exec()
@@ -12,10 +14,18 @@ router.get("/", (req, res) => {
     return;
   }
 
+  if (page) {
+    console.log('---->page', page);
+    const books = await Book.find({})
+      .skip(10 * (page - 1))
+      .limit(10);
+    return res.status(200).send(books);
+  }
+
   Book.findAll()
     .then((books) => {
       if (!books.length)
-        return res.status(404).send({ err: "Books not found" });
+        return res.status(404).send({ err: 'Books not found' });
       res.status(200).send(books);
     })
     .catch((err) => res.status(500).send(err));
@@ -23,32 +33,32 @@ router.get("/", (req, res) => {
 
 // 관리자에서 db 업데이트
 // 책 정보 initialize 시 poster 정보도 받아오기
-router.post("/update", (req, res) => {
+router.post('/update', (req, res) => {
   Book.deleteMany().then(() => {
     Book.insertMany(req.body).then((r) => {
-      res.json({ result: "ok" });
+      res.json({ result: 'ok' });
       axios.post(`http://localhost:${process.env.PORT}/posters/update`);
     });
   });
 });
 
 // 대여/반납
-router.put("/update", (req, res) => {
+router.put('/update', (req, res) => {
   Book.update({ isbn: req.body.isbn }, { lender: req.body.lender }).then(
     (resp) => {
-      res.json({ result: "update success" });
+      res.json({ result: 'update success' });
     }
   );
 });
 
-router.delete("/", (req, res) => {
+router.delete('/', (req, res) => {
   Book.deleteMany().then(() => {
-    res.json({ result: "delete success" });
+    res.json({ result: 'delete success' });
   });
 });
 
-router.get("/test", (req, res) => {
-  res.status(200).send("Tested");
+router.get('/test', (req, res) => {
+  res.status(200).send('Tested');
 });
 
 module.exports = router;
